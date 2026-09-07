094d3d41d3b75d712e9b68a1c47ba4c5d2b3c674
Ilaria Cogerino <ilaria.cogerino@gmail.com>
2026-09-07 14:49:21 +0200

    Add files via upload

---
+++ Tecniche-di-programmazione/MetroParis/database/DAO.py
@@ -0,0 +1,22 @@
+from database.DB_connect import DBConnect
+from model.fermata import Fermata
+
+
+class DAO():
+
+    @staticmethod
+    def getAllFermate():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT * FROM fermata"
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Fermata(**row))
+        cursor.close()
+        conn.close()
+        return result
+
---
+++ Tecniche-di-programmazione/MetroParis/model/fermata.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Fermata:
+    id_fermata: int
+    nome: str
+    coordX: int
+    coordY: int
+
+
+    def __hash__(self):
+        return self.id_fermata
+
+    def __str__(self):
+        return f"{self.nome}"
+
+    def __eq__(self, other):
+        return self.id_fermata == other.id_fermata
